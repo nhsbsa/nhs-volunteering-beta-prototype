ed046f159000f4428c2b8fdf51d14998ea9c2726
emma-nhs-vol <emma.axelsson@nhsbsa.nhs.uk>
2023-09-07 08:35:53 +0100

    /v3 - copy change in what its like, changes to routing for r2, general changes to r2 recruiter journey, including copy

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+// R1 BRANCHING
+
   // remote - then go to address if 'no'
 
   router.post('/remote-answer', function (req, res) {
@@ -60,6 +62,68 @@ router.post('/contact-details-answer', function (req, res) {
     res.redirect('/r1/questions/contact-details-change')
   } else {
     res.redirect('/r1/questions/support-volunteers')
+  }
+})
+
+// R2 BRANCHING
+
+// Add your routes here - above the module.exports line
+
+  // remote - then go to address if 'no'
+
+  router.post('/r2-remote-answer', function (req, res) {
+
+    // Get the answer from session data
+    const r2remote = req.session.data['r2-remote']
+  
+    if (r2remote === 'no') {
+      res.redirect('/r2/questions/address')
+    } else {
+      res.redirect('/r2/questions/availability')
+    }
+  })
+  
+  module.exports = router;
+
+// choose address - if "the address is not listed here"
+
+router.post('/r2-choose-address-answer', function (req, res) {
+
+    // Get the answer from session data
+    const r2chooseAddress = req.session.data['r2-choose-address']
+  
+    if (r2chooseAddress === 'not-listed') {
+      res.redirect('/r2/questions/manual-address')
+    } else {
+      res.redirect('/r2/questions/another-location')
+    }
+  })
+
+// add another location - if "answer is yes"
+
+router.post('/r2-another-location-answer', function (req, res) {
+
+    // Get the answer from session data
+    const r2anotherLocation = req.session.data['r2-another-location']
+  
+    if (r2anotherLocation === 'yes') {
+      res.redirect('/r2/questions/address')
+    } else {
+      res.redirect('/r2/questions/check-location')
+    }
+  })
+
+  // contact details - if "answer is yes"
+
+router.post('/r2-contact-details-answer', function (req, res) {
+
+  // Get the answer from session data
+  const r2contactDetails = req.session.data['r2-contact-details']
+
+  if (r2contactDetails === 'yes') {
+    res.redirect('/r2/questions/contact-details-change')
+  } else {
+    res.redirect('/r2/questions/support-volunteers')
   }
 })
 
